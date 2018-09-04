d997ce89202bf160c8a957eb50da5fc8a490d171
Cedric David <cedric.david@btopenworld.com>
2018-09-04 01:46:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+
+  end
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+     @@all.each do |dog|
+       puts "#{dog.name}"
+     end
+  end
+end
